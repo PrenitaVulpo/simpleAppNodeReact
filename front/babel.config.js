23ba3eb9a463eae03b7c2ad98df056abf967c5c8
PrenitaVulpo <guilhermepdl97@protonmail.com>
2020-12-09 12:23:58 -0300

    configurado o webpack server

--- front/babel.config.js
+++ front/babel.config.js
@@ -1,6 +1,15 @@
+//Aqui ficam as configurações relacioadas ao babel, ou seja, 
+//como será convertido o código react para um que o 
+//navegador entenda
+
 module.exports = {
+    //conjuntos de configurações criados por terceiros
     presets: [
+        //converte o código JS mais recente para um mais antigo, 
+        //levando em consideração o navegador que está rodando a aplicação
         '@babel/preset-env',
+        //adiciona as funcionalidades do react na conversão do JS
+        //como por exemplo, adicionar HTML dentro do JS
         '@babel/preset-react'
     ]
 }
--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,16 +1,34 @@
- const path = require('path');
+//Automatiza o processor de detecção do tipo de arquivo para carregamento
+//do loader equivalente
+
+
+//usado para definir caminho de forma que funcione em todos os SOs
+const path = require('path');
 
 module.exports = {
-    entry: path.resolve(_dirname, 'src', 'index.js'),
+    //define o arquivo de entrada da aplicação
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    //define o arquivo de saída da aplicação, quando já transpilada
     output: {
-        path: path.resolve(_dirname, 'public'),
+        //diz o caminho do destino
+        path: path.resolve(__dirname, 'public'),
+        //diz o nome do arquivo
         filename: 'bundle.js'
     },
+    devServer: {
+        //indica onde estão os arquivos públicos da aplicação
+        contentBase: path.resolve(__dirname, 'public')
+    },
+    //define os loaders para cada tipo de arquivo
     module: {
+        //define as regras para conversão
         rules: [
             {
+                //OBRIGATÒRIO: olha a extensão do arquivo
                 test: /\.js$/,
+                //indica alguma pasta a não ser testada
                 exclude: /node_modules/,
+                //OBRIGATÒRIO: indica o loader a ser utilisado para o tipo de arquivo definido
                 use: {
                     loader: 'babel-loader'
                 }
